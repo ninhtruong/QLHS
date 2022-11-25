306647f2f74d93e1c5ea405bbf9f8def70d2b178
truongnhatninh <truongnhatninh2709@gmail.com>
2022-11-25 10:14:21 +0700

    Thêm model: HocSinh, LopHoc, Mon Hoc

---
+++ QLHS/Model/HocSinh.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHS.Model
+{
+    public class HocSinh
+    {
+        public int MaHocSinh { get; set; }
+        public int MaLopHoc { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string DiaChi { get; set; }
+        public string Email { get; set; }
+        public float TBHK1 { get; set; }
+        public float TBHK2 { get; set; }
+    }
+}
---
+++ QLHS/Model/LopHoc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHS.Model
+{
+    public class LopHoc
+    {
+        public int MaLop { get; set; }
+        public int MaKhoi { get; set; }
+        public string TenLH { get; set; }
+        public int SiSoToiDa { get; set; }
+        public float Tyle { get; set; }
+    }
+}
