5ec1a455dda23b32a0796f09568aabfe1398fbf3
truongnhatninh2709@gmail.com <phglong1999@gmail.com>
2022-12-27 00:37:53 +0700

    Thêm bảng điểm (BUS + DAO + DTO)

---
+++ QLHS/BUS/BangDiemMonBUS.cs
@@ -0,0 +1,18 @@
+﻿using QLHSC3.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHSC3.BUS
+{
+    class BangDiemMonBUS
+    {
+        private BangDiemMonDAO adapterDAO = new BangDiemMonDAO();
+        public bool InsertMarkTable_BUS(int maHS, int maDiem, int maMH, int maHK)
+        {
+            return adapterDAO.InsertMarkTable(maHS, maDiem, maMH, maHK);
+        }
+    }
+}
